9077c6105700eb9685d20a1ce5802e1c129b477b
Irina Fedorova <skywalker2718@gmail.com>
2019-07-30 10:07:33 +0300

    Обновлены файлы front-page css/main.css scss/main.scss img/bg.svg js/common.js news.html typography.html

--- js/common.js
+++ js/common.js
@@ -10,39 +10,39 @@
 		})
 
 		/*Подключение к отзывам на Главной*/
-			$(".reviews-list .owl-carousel").owlCarousel ({
-				items: 2,
-				nav: true,
-				margin: 30,
-				navText: false,
-				responsive: {
-					0:{
-						items: 1,
-					},
-					768:{
-						items: 2
-					}
+		$(".reviews-list .owl-carousel").owlCarousel ({
+			items: 2,
+			nav: true,
+			margin: 30,
+			navText: false,
+			responsive: {
+				0:{
+					items: 1,
+				},
+				768:{
+					items: 2
 				}
-			})
-			$(".bestsellers .owl-carousel").owlCarousel ({
-				nav: true,
-				margin: 30,
-				navText: false,
-				autoHeight:true,
-				responsive: {
-					0:{
-						items: 1
-					},
-					768:{
-						items: 2
-					},				
-					992:{
-						items: 3
-					},
-					1199:{
-						items: 5
-					}
+			}
+		})
+		$(".bestsellers .owl-carousel").owlCarousel ({
+			nav: true,
+			margin: 30,
+			navText: false,
+			autoHeight:true,
+			responsive: {
+				0:{
+					items: 1
+				},
+				768:{
+					items: 2
+				},				
+				992:{
+					items: 3
+				},
+				1199:{
+					items: 5
 				}
-			})
+			}
+		})
 	});
 })(jQuery);
