81433ce328c721dc6a2733143a2759a6a6545caa
Irina Fedorova <skywalker2718@gmail.com>
2019-08-01 17:07:07 +0300

    Добавлены обертки контентной области и обертки к блокам сортировки

--- functions.php
+++ functions.php
@@ -143,8 +143,9 @@ function fleetservice_scripts() {
 	wp_enqueue_style( 'fleetservice-main', get_template_directory_uri() . '/css/main.css' );
 	
 	wp_enqueue_script( 'owl-carousel', get_template_directory_uri() . '/libs/owl.carousel/dist/owl.carousel.min.js', array('jquery'), null, true );
+	if (is_category()) {
 	wp_enqueue_script( 'masonry', get_template_directory_uri() . '/libs/masonry.pkgd.js', array(''), time(), true );
-	
+	}
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
