87ad13b98220f5b36dc625443b9472a5d2536210
Irina Fedorova <skywalker2718@gmail.com>
2019-08-02 15:01:20 +0300

    Форма поиска вынесена в отдельный файл

---
+++ searchform.php
@@ -0,0 +1,4 @@
+<form role="search" method="get" class="search-form" action="http://fleetservice.local/">
+	<span class="screen-reader-text"><?php _e('Найти:','fleetservice'); ?></span>
+	<input type="search" class="search-field" placeholder="" value="<?php echo get_search_query(); ?>" name="s">
+</form>
