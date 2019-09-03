a02e1995eb22d0e636ea36e887dd9562ad668e55
Груздева Светлана <gruzdeva.s@outlook.com>
2019-09-03 16:35:52 +0300

    Верстка одной новости

---
+++ template-parts/content-news.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package fleetservice
+ */
+
+?>
+<div class="container">
+	<article id="post-<?php the_ID(); ?>" <?php post_class('entry clear'); ?>>
+
+		<?php 
+		if ( 'post' === get_post_type() ) :
+			?>
+			<div class="entry-meta text-uppercase mb-2">
+				<?php
+				fleetservice_posted_on();
+				?>
+			</div><!-- .entry-meta -->
+		<?php endif; ?>
+
+		<header class="entry-header">
+			<?php
+			if ( is_singular() ) :
+				the_title( '<h1 class="entry-title">', '</h1>' );
+			else :
+				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			endif; ?>		
+
+		</header><!-- .entry-header -->
+
+		<div class="entry-meta mb-3">Рубрика: <a href="/">Уют в доме</a></div><!--/.entry-meta-->
+		
+		<?php fleetservice_post_thumbnail(); ?>
+
+		<div class="entry-content">
+			<?php
+			the_content( sprintf(
+				wp_kses(
+					/* translators: %s: Name of current post. Only visible to screen readers */
+					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'fleetservice' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+				get_the_title()
+			) );
+
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'fleetservice' ),
+				'after'  => '</div>',
+			) );
+			?>
+		</div><!-- .entry-content -->		
+	</article><!-- #post-<?php the_ID(); ?> -->
+
+	<!-- Здесь будет блок Поделиться -->
+	<div class="fleet_share">
+		<div class="fleet_share__title">Поделиться</div>
+	</div><!--/.fleet_share-->
+
+</div><!--/.container-->
